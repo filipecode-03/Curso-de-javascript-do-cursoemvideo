41855a94340f227dc89f7bb3e02244b0b8d4fc90
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-02-23 16:41:51 -0400

    inciei a primeira aula do js

---
+++ aula04/index.js
@@ -0,0 +1,11 @@
+window.alert('Minha primeira mensagem!')
+window.confirm('Esta gostando de JS?')
+window.prompt('Qual é o seu nome?')
+
+var n1 = 5
+var n2 = 8.5 
+var n3 = 15
+
+var s1 = "JavaScript"
+var s2 = 'Curso em Video'   
+var s3 = 'Gunabara'
