d0bae1b6e2d430f21123264a10bfd8b352a1b598
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-02-29 14:05:25 -0400

    aprendendo sobre variáveis

--- aula6/ex003/index.js
+++ aula6/ex003/index.js
@@ -1,4 +1,4 @@
 var n1 = Number(window.prompt('digite um numero: '))
 var n2 = Number(window.prompt('Digite outro numero: '))
 var s = n1 + n2
-window.alert('A soma do valores é' + s)
+window.alert('A soma do valores é' + String(s))
