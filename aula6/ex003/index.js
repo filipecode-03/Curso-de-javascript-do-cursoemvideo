ad3b6b01d3ee7887e5429b3e67b78a6e3c57afed
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-03-01 15:04:40 -0400

    mexendo com variáveis

--- aula6/ex003/index.js
+++ aula6/ex003/index.js
@@ -1,4 +1,4 @@
 var n1 = Number(window.prompt('digite um numero: '))
 var n2 = Number(window.prompt('Digite outro numero: '))
 var s = n1 + n2
-window.alert('A soma do valores é' + String(s))
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${s}`)
